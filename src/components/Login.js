313c793f60d96e253391eb652a4fe4af02da56bc
Bhaskar Paul <paulbhaskar007@gmail.com>
2021-10-20 11:45:52 +0530

    firebase config updated ...

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,26 +5,28 @@ import { auth } from "../Firebase";
 import firebase from "firebase/app";
 
 const Login = () => {
-  return (
-    <div id="login-page">
-      <div id="login-card">
-        <h2>Welcome to UniChat!</h2>
-        <div
-          className="login-button google"
-          onClick={() =>
-            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-          }
-        >
-          <GoogleOutlined /> Sign in with Google
-        </div>
-        {/* <br />
+    return (
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to UniChat!</h2>
+                <div
+                    className="login-button google"
+                    onClick={() =>
+                        auth.signInWithRedirect(
+                            new firebase.auth.GoogleAuthProvider()
+                        )
+                    }
+                >
+                    <GoogleOutlined /> Sign in with Google
+                </div>
+                {/* <br />
         <br />
         <div className="login-button facebook">
           <FacebookOutlined /> Sign in with Facebook
         </div> */}
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default React.memo(Login);
